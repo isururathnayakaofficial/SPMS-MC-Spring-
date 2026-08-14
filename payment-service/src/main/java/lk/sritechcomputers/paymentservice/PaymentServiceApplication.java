d0183afbf6c3b0523251d55e486df51f9a211108
Isuru Rathnayaka <rathnayakaisuru422@gmail.com>
2026-08-14 15:41:26 +0530

    fix: server issue solved

--- payment-service/src/main/java/lk/sritechcomputers/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/lk/sritechcomputers/paymentservice/PaymentServiceApplication.java
@@ -2,11 +2,14 @@ package lk.sritechcomputers.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(PaymentServiceApplication.class, args);
     }
 
