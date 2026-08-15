3a5c4954d3255cd2adf1b5889290701926478364
Isuru Rathnayaka <rathnayakaisuru422@gmail.com>
2026-08-15 22:41:20 +0530

    fix: config server issue fixed

---
+++ car-parking-service/src/main/java/lk/sritechcomputers/carparkingservice/CarParkingServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.sritechcomputers.carparkingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarParkingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarParkingServiceApplication.class, args);
+    }
+
+}
